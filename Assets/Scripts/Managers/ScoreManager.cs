c0f25fc26dd6ce7e15f712d391faaca167df67be
unknown <ap.harko@gmail.com>
2017-11-25 09:09:26 +0200

    scoremanager added

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+    public int score;
+
+
+	// Use this for initialization
+	void Awake () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SetScore(int newScore)
+    {
+        score = newScore;
+    }
+
+    void AddScore (int amount)
+    {
+        SetScore(amount + score);
+    }
+
+    void ReduceScore (int amount)
+    {
+        int newScore = score - amount;
+        if (newScore < 0)
+        {
+            SetScore(0);
+        } else
+        {
+            SetScore(score - amount);
+        }
+    }
+
+
+}
