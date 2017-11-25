66cb3420a1d6c28ac35d976efe98a4d10eb45182
unknown <ap.harko@gmail.com>
2017-11-25 12:32:30 +0200

    functionalities, rotation

---
+++ Assets/Scripts/EquipmentBehavior.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+public class EquipmentBehavior : VRTK_InteractableObject
+{
+    float spinSpeed = 0f;
+    Transform rotator;
+    bool isBeingTouched;
+    float prevControllerZ;
+
+    public override void StartUsing(VRTK_InteractUse usingObject)
+    {
+        base.StartUsing(usingObject);
+        print(usingObject.transform.rotation.eulerAngles.z);
+        spinSpeed = 360f;
+        isBeingTouched = true;
+
+    }
+
+    public override void StopUsing(VRTK_InteractUse usingObject)
+    {
+        base.StopUsing(usingObject);
+        spinSpeed = 0f;
+        isBeingTouched = false;
+    }
+
+    protected void Start()
+    {
+        rotator = GameObject.Find("Graphics").transform.Find("Cube");
+        isBeingTouched = false;
+        prevControllerZ = 0;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        
+        if (isBeingTouched)
+        {
+            float controllerAngle = usingObject.transform.rotation.eulerAngles.z;
+            if (prevControllerZ != 0)
+            {
+                float diff = controllerAngle - prevControllerZ;
+                rotator.transform.Rotate(new Vector3(0f, 0f, diff));
+
+            }
+            prevControllerZ = controllerAngle;
+        }
+
+    }
+}
--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -1,20 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour {
 
-    public int score;
-
+    int score;
+    public Text scoreText;
 
 	// Use this for initialization
 	void Awake () {
-        score = 0;
-	}
+        score = 10;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        scoreText.text = "Score: " + score;
 	}
 
     void SetScore(int newScore)
